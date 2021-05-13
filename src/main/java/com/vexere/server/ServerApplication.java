ec5b7e743b9951b0703f96f5eea334dde6856525
thytran38 <ax4409h@gmail.com>
2021-05-14 00:25:58 +0700

    first commit

---
+++ src/main/java/com/vexere/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.vexere.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
